9d7033dca9a129351a83d58991d9268bce7613b0
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-14 16:04:05 +0100

    0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ *bubble_sort - sorts an array according to bubble sort algo
+ *@arr: array to sort
+ *@n: size of arrray
+ */
+void bubble_sort(int arr[], int n)
+{
+	int i = 0;
+	int temp;
+	int again = 0;
+	int iter;
+
+	iter = n - 1;
+
+	do {
+		again = 0;
+		i = 0;
+		for (; i < iter; ++i)
+		{
+			if (arr[i + 1] < arr[i])
+			{
+				again = 1;
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+		--iter;
+	} while (again);
+}
+
