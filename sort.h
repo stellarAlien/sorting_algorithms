c33c72c1cde47d4cb6825958d8009c5e0947bf95
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-15 17:39:10 +0100

    select

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-#include<stdlib.h>
 /**
  *  * struct listint_s - Doubly linked list node
  *   *
@@ -17,5 +16,6 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 #endif
