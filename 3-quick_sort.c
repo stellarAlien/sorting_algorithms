b612c934cdfcd4017dce72aab12b93991b66c9af
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-15 21:59:43 +0100

    3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,53 +1,62 @@
 #include"sort.h"
 /**
- * permut - permutes two values of array in positions i & j
- * @array: array to operate on
+ *swap - permutes two values of array in positions i & j
+ * @arr: array to operate on
  *@i: index of first element
  *@j: index of other element
  */
 void swap(int arr[], int i, int j)
 {
 	int x;
+
 	x = arr[i];
 	arr[i] = arr[j];
 	arr[j] = x;
 
 
 }
-
+/**
+ * partition - sets the left side of the array according to pivot
+ * @arr: array to parition
+ * @low: lower bound
+ * @high: higher bound
+ * @size: size of array
+ * Return: index of first elemnt of unsorted array
+ */
 int partition(int arr[], int low, int high, size_t size)
 {
 	int j;
 	int pivot = arr[high];
-	int i = (low - 1);
-		 
-	for(j = low; j <= high- 1; j++)
+	int i;
+
+	i = (low - 1);
+	for (j = low; j <= high - 1; j++)
 	{
 		if (arr[j] <= pivot)
 		{
 			i++;
-			swap(arr,i,j);
+			swap(arr, i, j);
 		}
-	}	
+	}
 	(void)size;
-	print_array(arr, size);
 	swap(arr, i + 1, high);
+	print_array(arr, size);
 	return (i + 1);
 }
 /**
  *quickSort - sortds array with recursive quicksort
+ *@arr: array to sort
  *@low: lower bound
  *@high: higher bound
+ *@size: size of arr
  */
-void quickSort(int arr[], int low, int high,size_t size)
+void quickSort(int arr[], int low, int high, size_t size)
 {
+	int pi;
 	(void)size;
 	if (low < high)
 	{
-	int pi; 
-		
 	pi = partition(arr, low, high, size);
-					 
 	quickSort(arr, low, pi - 1, size);
 	quickSort(arr, pi + 1, high, size);
 	}
@@ -58,9 +67,9 @@ void quickSort(int arr[], int low, int high,size_t size)
  *@size: size of array
  */
 
-void quick_sort(int *array,size_t size)
+void quick_sort(int *array, size_t size)
 {
-	if(size < 2)
+	if (size < 2)
 		return;
 	quickSort(array, 0, size - 1, size);
 }
