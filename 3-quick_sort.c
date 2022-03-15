3a89438186d073343108810a12d7ec68df4e9a9a
stellarAlien <saifgaida@gmail.com>
2022-03-15 15:52:28 +0100

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,77 +1,66 @@
-#include"sort.h"
+#include "sort.h"
 /**
- *swap - permutes two values of array in positions i & j
- * @arr: array to operate on
- *@i: index of first element
- *@j: index of other element
+ * partition - hi
+ * @array: hi
+ * @first: hi
+ * @last: hi
+ * @size: hi
+ * Return: hi
  */
-void swap(int arr[], int i, int j)
+int partition(int *array, int first, int last, size_t size)
 {
-	int x;
+	int pivot = array[last], x = 0;
+	int i = (first - 1), j;
 
-	x = arr[i];
-	arr[i] = arr[j];
-	arr[j] = x;
-
-
-}
-/**
- * partition - sets the left side of the array according to pivot
- * @arr: array to parition
- * @low: lower bound
- * @high: higher bound
- * @size: size of array
- * Return: index of first elemnt of unsorted array
- */
-int partition(int arr[], int low, int high, size_t size)
-{
-	int j;
-	int pivot = arr[high];
-	int i;
-
-	i = (low - 1);
-	for (j = low; j <= high - 1; j++)
+	for (j = first; j < last; j++)
 	{
-		if (arr[j] <= pivot)
+		if (array[j] <= pivot)
 		{
 			i++;
-			swap(arr, i, j);
+			if (i != j)
+			{
+			x = array[i];
+			array[i] = array[j];
+			array[j] = x;
+			print_array(array, size);
+			}
 		}
 	}
-	(void)size;
-	swap(arr, i + 1, high);
-	print_array(arr, size);
+	if (i + 1 != last)
+	{
+		x = array[i + 1];
+		array[i + 1] = array[last];
+		array[last] = x;
+		print_array(array, size);
+	}
 	return (i + 1);
 }
 /**
- *quickSort - sortds array with recursive quicksort
- *@arr: array to sort
- *@low: lower bound
- *@high: higher bound
- *@size: size of arr
+ * quickSort - bubble sort
+ * @array: array of integers
+ * @first: hi
+ * @last: hi
+ * @size: size of array
  */
-void quickSort(int arr[], int low, int high, size_t size)
+void quickSort(int *array, int first, int last, size_t size)
 {
 	int pi;
-	(void)size;
-	if (low < high)
-	{
-	pi = partition(arr, low, high, size);
-	quickSort(arr, low, pi - 1, size);
-	quickSort(arr, pi + 1, high, size);
-	}
-	}
-/**
- *quick_sort - sorts array with quick sort method(my fav)
- *@array: array to sort
- *@size: size of array
- */
 
+	if (first < last)
+	{
+		pi = partition(array, first, last, size);
+		quickSort(array, first, pi - 1, size);
+		quickSort(array, pi + 1, last, size);
+	}
+}
+/**
+ * quick_sort - bubble sort
+ * @array: array of integers
+ * @size: size of array
+ */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
+	if (size < 1)
 		return;
 	quickSort(array, 0, size - 1, size);
 }
-
-
